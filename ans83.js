71a4b922d444197cf15d4bb05facaef08ba03d2c
asadullah48 <asadullahshafique@hotmail.com>
2024-05-25 15:49:36 +0500

    Initial commit

---
+++ ans83.js
@@ -0,0 +1,10 @@
+// Q 83
+// This function changes a string to uppercase and then to lowercase
+function convertCase(str) {
+    var upperStr = str.toUpperCase(); // Converts to uppercase
+    var lowerStr = str.toLowerCase(); // Converts to lowercase
+    console.log("Uppercase:", upperStr, "Lowercase:", lowerStr);
+}
+// Example: Changing the case of "Hello World"
+convertCase("i am learning coding"); // Outputs: "Uppercase: HELLO WORLD Lowercase: hello world"
+// We're showing the same text in all uppercase and then all lowercase.
---
+++ ans83.ts
@@ -0,0 +1,11 @@
+// Q 83
+// This function changes a string to uppercase and then to lowercase
+function convertCase(str: string) {
+    let upperStr = str.toUpperCase(); // Converts to uppercase
+    let lowerStr = str.toLowerCase(); // Converts to lowercase
+  
+    console.log("Uppercase:", upperStr, "Lowercase:", lowerStr);
+  }
+  convertCase("i am learning coding"); //
+  // Example: Changing the case of "Hello World"
+  // We're showing the same text in all uppercase and then all lowercase.
